0c2d2e3a6dfc2ca7bdd588a69d09bd4e563dc392
Shanmukh459 <saikrrish459@gmail.com>
2024-05-03 11:38:49 -0700

    printing posts on the screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import { Posts } from './components/Posts'
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -17,11 +18,11 @@ function App() {
     fetchPosts()
   }, [])
 
-  console.log(posts)
 
   return (
-    <div className='container'>
-      <h1>Hello!</h1>
+    <div className='container mt-5'>
+      <h1 className='text-primary mb-3'>My Blog</h1>
+      <Posts posts={posts} loading={loading} />
     </div>
   )
 }
